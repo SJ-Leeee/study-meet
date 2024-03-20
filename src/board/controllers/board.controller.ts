a0c36543c07c80dc3f9c2a46939d12e523192577
leeseungjun <cjstkrak@gmail.com>
2024-03-20 17:11:09 +0900

    fix: 게시물 스웨거작성

--- src/board/controllers/board.controller.ts
+++ src/board/controllers/board.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
   Param,
   Post,
   Put,
@@ -16,12 +17,35 @@ import { PostBoardDto } from '../dto/postBoardReq.dto';
 import { User } from 'src/common/decorator/user.decorator';
 import { BoardService } from '../services/board.service';
 import { ReqUserDto } from 'src/common/dto/reqUser.dto';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiConsumes,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Board')
 @Controller('board')
 export class BoardController {
   constructor(private readonly boardService: BoardService) {}
 
+  // 게시물 생성 api
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    summary: '게시물 생성',
+    description: 'jwt로 인증된 사용자가 게시물을 생성, 이미지 선택',
+  })
+  @ApiBody({ type: PostBoardDto })
+  @ApiBearerAuth()
+  @ApiResponse({
+    status: 201,
+    description: '게시물 생성',
+  })
+  @ApiConsumes('multipart/form-data')
+  @HttpCode(201)
   @UseInterceptors(
     FilesInterceptor('images', 10, {
       limits: { fileSize: 1024 * 1024 * 10 },
@@ -37,24 +61,70 @@ export class BoardController {
     return { message: 'success' };
   }
 
+  // 게시물 전체 조회 api
+  @ApiOperation({
+    summary: '게시물 전체 조회',
+    description: '게시물 전체 조회',
+  })
+  @ApiResponse({
+    status: 200,
+    description: '게시물 전체 조회 성공',
+  })
+  @HttpCode(200)
   @Get()
   async getAllBoards() {
     return await this.boardService.getAllBoards();
   }
 
+  // 특정 게시물 조회 api
+  @ApiOperation({
+    summary: '게시물 생성',
+    description: 'jwt로 인증된 사용자가 게시물을 생성, 이미지 선택',
+  })
+  @ApiParam({ name: 'id', description: '게시물 id로 검색', example: 5 })
+  @ApiResponse({
+    status: 200,
+    description: '게시물 조회',
+  })
+  @HttpCode(200)
   @Get('/:id')
   async getBoard(@Param('id') id: string) {
     return await this.boardService.getBoard(+id);
   }
 
+  // 게시물 삭제
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    summary: '게시물 삭제',
+    description: 'jwt로 인증된 관리자 또는 작성자가 게시물을 삭제',
+  })
+  @ApiParam({ name: 'id', description: '게시물 id로 검색', example: 5 })
+  @ApiBearerAuth()
+  @ApiResponse({
+    status: 200,
+    description: '게시물 삭제',
+  })
+  @HttpCode(200)
   @Delete('/:id')
   async deleteBoard(@Param('id') boardId: string, @User() user: ReqUserDto) {
     await this.boardService.deleteBoard(user, +boardId);
     return { message: 'success' };
   }
 
+  // 게시물 수정 api
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    summary: '게시물 삭제',
+    description: 'jwt로 인증된 관리자 또는 작성자가 게시물을 수정',
+  })
+  @ApiParam({ name: 'id', description: '게시물 id로 검색', example: 5 })
+  @ApiBearerAuth()
+  @ApiResponse({
+    status: 200,
+    description: '게시물 수정',
+  })
+  @ApiConsumes('multipart/form-data')
+  @HttpCode(200)
   @UseInterceptors(
     FilesInterceptor('images', 10, {
       limits: { fileSize: 1024 * 1024 * 10 },
--- src/board/controllers/comment.controller.ts
+++ src/board/controllers/comment.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
   Param,
   Post,
   Put,
@@ -13,12 +14,32 @@ import { JwtAuthGuard } from 'src/guards/jwtAuth.guard';
 import { User } from 'src/common/decorator/user.decorator';
 import { ReqUserDto } from 'src/common/dto/reqUser.dto';
 import { PostCommentDto } from '../dto/postCommentReq.dto';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Comment')
 @Controller('board/:boardId/comment')
 export class CommentController {
   constructor(private readonly cmtService: CommentService) {}
 
+  // 댓글작성
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    summary: '댓글 생성',
+    description: 'jwt로 인증된 사용자가 댓글 작성',
+  })
+  @ApiParam({ name: 'boardId', description: '게시물 id로 검색', example: 5 })
+  @ApiBearerAuth()
+  @ApiResponse({
+    status: 201,
+    description: '댓글 생성',
+  })
+  @HttpCode(201)
   @Post()
   async postComment(
     @Param('boardId') boardId: string,
@@ -29,12 +50,37 @@ export class CommentController {
     return { message: 'success' };
   }
 
+  // 특정 게시물 댓글 조회 api
+  @ApiOperation({
+    summary: '댓글 전체 조회',
+    description: '특정 게시물의 댓글을 전체 조회',
+  })
+  @ApiParam({ name: 'boardId', description: '게시물 id로 검색', example: 5 })
+  @ApiBearerAuth()
+  @ApiResponse({
+    status: 200,
+    description: '댓글 조회',
+  })
+  @HttpCode(200)
   @Get()
   async getComments(@Param('boardId') boardId: string) {
     return await this.cmtService.getAllComments(+boardId);
   }
 
+  // 특정 게시물의 특정 댓글 조회 api 쓸데가 없을 듯
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    summary: '댓글 전체 조회',
+    description: '특정 게시물의 특정 댓글을 조회',
+  })
+  @ApiParam({ name: 'boardId', description: '게시물 id로 검색', example: 5 })
+  @ApiParam({ name: 'commentId', description: '댓글 id로 검색', example: 5 })
+  @ApiBearerAuth()
+  @ApiResponse({
+    status: 200,
+    description: '특정 댓글 조회',
+  })
+  @HttpCode(200)
   @Put('/:commentId')
   async editComment(
     @Param() ids,
@@ -50,7 +96,21 @@ export class CommentController {
 
     return { message: 'success' };
   }
+
+  // 특정 댓글 삭제 api
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    summary: '댓글 삭제',
+    description: 'jwt로 인증된 관리자나 작성자가 댓글 삭제',
+  })
+  @ApiParam({ name: 'boardId', description: '게시물 id로 검색', example: 5 })
+  @ApiParam({ name: 'commentId', description: '댓글 id로 검색', example: 5 })
+  @ApiBearerAuth()
+  @ApiResponse({
+    status: 200,
+    description: '특정 댓글 삭제',
+  })
+  @HttpCode(200)
   @Delete('/:commentId')
   async deleteComment(@Param() ids, @User() user: ReqUserDto): Promise<any> {
     await this.cmtService.deleteCommment(user, +ids.boardId, +ids.commentId);
