a1f916a65eb1e5fb6d8cff4e1766ef34a251b12b
leeseungjun <cjstkrak@gmail.com>
2024-03-20 17:26:13 +0900

    fix : chat스웨거 작성

--- src/chat/controllers/chat.controller.ts
+++ src/chat/controllers/chat.controller.ts
@@ -1,15 +1,44 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { ChatService } from '../services/chat.service';
 import { JwtAuthGuard } from 'src/guards/jwtAuth.guard';
 import { User } from 'src/common/decorator/user.decorator';
 import { ReqUserDto } from 'src/common/dto/reqUser.dto';
 import { ChatReqDto } from '../dto/chatReq.dto';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Chat')
 @Controller('chat')
 export class ChatController {
   constructor(private readonly chatService: ChatService) {}
 
+  // 채팅내역 전체조회 api
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    summary: '채팅내역 조회',
+    description: 'jwt로 인증된 작성자가 채팅내역 전체조회',
+  })
+  @ApiParam({ name: 'roomId', description: '방 id로 검색', example: 5 })
+  @ApiBearerAuth()
+  @ApiResponse({
+    status: 200,
+    description: '채팅 조회',
+  })
+  @HttpCode(200)
   @Get('/:roomId')
   async findChatInRoom(
     @User() user: ReqUserDto,
@@ -18,7 +47,20 @@ export class ChatController {
     return await this.chatService.findChatInRoom(user, +roomId);
   }
 
+  // 채팅 생성 api
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    summary: '채팅생성',
+    description: 'jwt로 인증된 작성자가 채팅작성',
+  })
+  @ApiParam({ name: 'roomId', description: '방 id로 검색', example: 5 })
+  @ApiBearerAuth()
+  @ApiBody({ type: ChatReqDto })
+  @ApiResponse({
+    status: 201,
+    description: '채팅 작성',
+  })
+  @HttpCode(201)
   @Post('/:roomId')
   async sendChatInRoom(
     @User() user: ReqUserDto,
--- src/chat/controllers/room.controller.ts
+++ src/chat/controllers/room.controller.ts
@@ -1,27 +1,67 @@
-import { Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { User } from 'src/common/decorator/user.decorator';
 import { ReqUserDto } from 'src/common/dto/reqUser.dto';
 import { JwtAuthGuard } from 'src/guards/jwtAuth.guard';
 import { RoomService } from '../services/room.service';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Room')
 @Controller('room')
 export class RoomController {
   constructor(private readonly roomService: RoomService) {}
 
-  // 내 룸 조회
+  // 내 룸 조회api
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    summary: '내가 속한 방 조회',
+    description: 'jwt로 인증된 사용자가 본인이 속한 모든 방 조회',
+  })
+  @ApiBearerAuth()
+  @ApiResponse({
+    status: 200,
+    description: '모든 방 조회',
+  })
+  @HttpCode(200)
   @Get()
   async findMyRooms(@User() user: ReqUserDto) {
     return await this.roomService.findAllRooms(+user.userId);
   }
 
-  // 룸 만들기
+  // 룸 만들기 api
   @UseGuards(JwtAuthGuard)
-  @Post('/:secondId')
+  @ApiOperation({
+    summary: '새로운 방 생성',
+    description: 'jwt로 인증된 사용자가 상대방과의 방 생성',
+  })
+  @ApiBearerAuth()
+  @ApiResponse({
+    status: 201,
+    description: '새로운 방 생성',
+  })
+  @HttpCode(201)
+  @ApiParam({
+    name: 'targetUserId',
+    description: '상대방의 userId',
+    example: 3,
+  })
+  @Post('/:targetUserId')
   async createRoom(
     @User() user: ReqUserDto,
-    @Param('secondId') secondId: string,
+    @Param('targetUserId') targetUserId: string,
   ) {
-    await this.roomService.createRoom(user, +secondId);
+    await this.roomService.createRoom(user, +targetUserId);
   }
 }
